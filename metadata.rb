a2e335c0726cb41a6caed98d113029c8bcfe97fc
David Pell <davidcpell@gmail.com>
2016-09-26 15:34:25 -0400

    fix metadata

--- metadata.rb
+++ metadata.rb
@@ -1,17 +1,9 @@
 name 'ws_users'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'David Pell'
+maintainer_email 'davidcpell@gmail.com'
 license 'all_rights'
-description 'Installs/Configures ws_users'
-long_description 'Installs/Configures ws_users'
-version '0.2.0'
+description 'Installs/Configures users for my workstation'
+long_description 'Installs/Configures users for my workstation'
+version '0.2.1'
 
 depends 'users', '~> 4.0.1'
-
-# If you upload to Supermarket you should set this so your cookbook
-# gets a `View Issues` link
-# issues_url 'https://github.com/<insert_org_here>/ws_users/issues' if respond_to?(:issues_url)
-
-# If you upload to Supermarket you should set this so your cookbook
-# gets a `View Source` link
-# source_url 'https://github.com/<insert_org_here>/ws_users' if respond_to?(:source_url)
